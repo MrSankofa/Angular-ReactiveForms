217e13d27b38f87c8d1c32a86e0cb1129ad6428b
Brett Cunningham <brett.cunningham@sbevision.com>
2020-02-05 08:40:24 -0500

    implemented bootstrap validation

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -16,9 +16,9 @@ export class CustomerComponent implements OnInit {
 
   ngOnInit() {
     this.customerForm = this.formBuilder.group({
-      firstName: ['', Validators.required],
-      lastName: [{value: 'n/a', disabled: true}, Validators.required],
-      email: ['', Validators.required],
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
       sendCatalog: true
     });
   }
